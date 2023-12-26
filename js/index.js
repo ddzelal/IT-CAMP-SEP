f5d156cd8ba0d3286739bb867099e0bdb0559873
Dzelal Dupljak <dzelal_d@live.com>
2023-12-26 20:48:04 +0100

    drugi cas js

---
+++ js/index.js
@@ -0,0 +1,43 @@
+console.log('Ovo je nas prvi cas!',2);
+
+
+
+// prosti!
+var ovoJeNumber = 23;
+var ovoJeBoolean = true;
+var ovoJeString = "Vekjko Sedi Pored Mervana"
+var ovoJeUndefined = undefined;
+var ovoJeNull = null;
+// Number,
+// Boolean,
+// String,
+// undefined,
+// null
+
+let brojdeset = 10;
+brojdeset = 12;
+brojdeset = "dwadwa";
+
+
+// slozeni
+
+var ovoJeArray = [2,"[]",true,"false",ovoJeString];
+// Array,
+// Object,
+// Function
+
+
+
+var osoba = {
+    ime:"Mervan",
+    prezume: "Zejenlovic",
+    godine:15
+}
+
+
+
+function test(e){
+console.log(e)
+}
+
+test(2)
