caed0b92905202881f8b32ac7bc421dee9662ac3
Dzelal Dupljak <dzelal_d@live.com>
2024-01-01 19:00:06 +0100

    domaci

--- js/index.js
+++ js/index.js
@@ -1,43 +1,154 @@
-console.log('Ovo je nas prvi cas!',2);
+// console.log('Ovo je nas prvi cas!',2);
 
 
 
-// prosti!
-var ovoJeNumber = 23;
-var ovoJeBoolean = true;
-var ovoJeString = "Vekjko Sedi Pored Mervana"
-var ovoJeUndefined = undefined;
-var ovoJeNull = null;
-// Number,
-// Boolean,
-// String,
-// undefined,
-// null
+// // prosti!
+// var ovoJeNumber = 23;
+// var ovoJeBoolean = true;
+// var ovoJeString = "Vekjko Sedi Pored Mervana"
+// var ovoJeUndefined = undefined;
+// var ovoJeNull = null;
+// // Number,
+// // Boolean,
+// // String,
+// // undefined,
+// // null
 
-let brojdeset = 10;
-brojdeset = 12;
-brojdeset = "dwadwa";
-
-
-// slozeni
-
-var ovoJeArray = [2,"[]",true,"false",ovoJeString];
-// Array,
-// Object,
-// Function
+// let brojdeset = 10;
+// brojdeset = 12;
+// brojdeset = "dwadwa";
 
 
+// // slozeni
 
-var osoba = {
-    ime:"Mervan",
-    prezume: "Zejenlovic",
-    godine:15
+// var ovoJeArray = [2,"[]",true,"false",ovoJeString];
+// // Array,
+// // Object,
+// // Function
+
+
+
+// var osoba = {
+//     ime:"Mervan",
+//     prezume: "Zejenlovic",
+//     godine:15
+// }
+
+
+
+// function test(e){
+// console.log(e)
+// }
+
+// test(2)
+
+
+// var praznastting = ["t","e","s","t"]
+// var test = 'test'
+// var ovojenull = null;
+// console.log(typeof test);
+// var auKemale = "2";
+// test = 2;
+
+
+
+
+// let dodeliVrednost = 23;
+
+
+// let plus = 2 + 2;
+// let minus = 2-2;
+// let mnozenje = 2*2;
+// let deljenje = 2/2;
+// let number = 2;
+// let string = "2";
+
+
+// let test1 =false || (number == string);
+
+// console.log(!test1);
+
+// let test2 = 2 <= 2;
+
+
+// console.log(test2)
+
+
+
+
+// let kemal = 150;
+
+// const cokolada = 120;
+
+
+
+// console.log(kemal >= cokolada);
+// kemal = kemal - cokolada;
+// console.log(kemal >= cokolada);
+// console.log(kemal)
+
+// let mojNovcanik = 100;
+
+// const kafa = 10;
+// const sok = 20;
+// const cigare = 250;
+
+
+
+
+
+// let kemal = prompt("Koliko ce kemal da ima para....");
+// const staZelisKupiti = prompt("Mozes kupiti 1.cokolada , 2.cigare, 3.sok").toLowerCase();
+// console.log(typeof kemal, "gore typeof od kemal")
+
+// const sok = 60;
+// const cokolada = 40;
+// const cigare = 480;
+
+// switch(staZelisKupiti){
+//     case "cokolada":
+//         if(kemal >= cokolada){
+//             kemal = kemal - cokolada;
+//             console.log(`Kupio si ${staZelisKupiti}, i ostalo ti je ${kemal}`)
+//             console.log(typeof kemal,"typeof od kemal")
+//         }
+//         else{
+//             console.log("Nemas dovoljno para....")
+//         }
+//         break;
+//     case "cigare":
+//         if(kemal >= cigare){
+//             kemal = kemal - cigare;
+//             console.log(`Kupio si ${staZelisKupiti}, i ostalo ti je ${kemal}`)
+//         }else {
+//             console.log("Nemas dovoljno para....")
+//         }
+//         break;
+//     case "sok":
+//         if(kemal >= sok){
+//             kemal = kemal - sok;
+//             console.log(`Kupio si ${staZelisKupiti}, i ostalo ti je ${kemal}`)
+//         }else {
+//             console.log("Nemas dovoljno para....")
+//         }
+//         break;
+//     default:
+//         console.log("Odustao si nisi hteo da kupis nista...")
+// }
+
+
+
+
+if(true && true){
+    console.log("uslov ispunjen")
+}
+else{
+    console.log("uslov nije ispunjen")
 }
 
 
 
-function test(e){
-console.log(e)
-}
-
-test(2)
+// domaci jeste da korisnika pitate koliko zeli novca da ima
+// napraviti takodje store gde mozete kupiti odredjenje artikle
+//ako korisnik uneste neki od ponudjenih onda se provera da li korisnik ima novca dovljno za to ako nema pitamo korisnika
+// da li zeli da se uzajmi i vi korisniku pozajmite onoliko koliko on unese 
