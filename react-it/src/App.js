23f65a2746c92b94de0971b4d1327aa35f59ba19
ddzelal <dzelal_d@live.com>
2024-05-13 20:53:07 +0200

    nadija

--- react-it/src/App.js
+++ react-it/src/App.js
@@ -7,25 +7,59 @@ const intialValue = 0;
 
 function App() {
   const [counter, setCounter] = useState(intialValue);
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
 
   const resetCounter = () => {
     setCounter(intialValue);
   }
 
 
+  const handleWhitchNumber = (numer) =>{
+    console.log(numer);
+    num1 === 0 ? handlleClickNum1(numer) : handlleClickNum2(numer);
+  }
+
+  console.log(num1, num2,"num1 > num2");
+
+  const handlleClickNum1 = (num1) => {
+    setNum1(num1);
+  }
+
+  const handlleClickNum2 = (num2) => {
+    setNum2(num2);
+  }
+
+  const handleCalculate = () => {
+    setCounter(num1 + num2)
+  }
+
+
+
+
+
 
   const calculatorElemnt = () => {
     return (
       <React.Fragment>
-        <button>1</button>
-        <button>2</button>
+        <div style={{display:'flex'}}>
+        <button onClick={()=> {handleWhitchNumber(1)}}>1</button>
+        <button onClick={()=> {handleWhitchNumber(2)}}>2</button>
         <button>3</button>
+        <button>+</button>
+        </div>
+        <div style={{display:'flex'}}>
         <button>4</button>
         <button>5</button>
         <button>6</button>
+        <button>-</button>
+        </div>
+        <div style={{display:'flex'}}>
         <button>7</button>
         <button>8</button>
         <button>9</button>
+        <button onClick={()=>{handleCalculate()}}>=</button>
+        </div>
         <button>0</button>
       </React.Fragment>
     )
@@ -45,7 +79,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img onClick={()=>console.log("Mervan je trodupli neradnik")} src={logo} className="App-logo" alt="logo" /> */}
+        {/* <img onClick={()=>console.log("Mervan je trodupli neradnik")} src={logo} classNa￼Enable access ➔
+me="App-logo" alt="logo" /> */}
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
